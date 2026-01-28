bed65e27c82f97316282b21e7509cba45440f8a3
Daniel Amado <damadop@miumg.edu.gt>
2026-01-28 17:04:42 -0600

    Codigo de C++, Diagrama de Flujo

---
+++ OneDrive/Documentos/Acividades Programacion 1/Hola Mundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << "\n" << endl;
+    cout << "Daniel Armando Amado Pelaez - 9959-25-3251" << "\n" << endl;
+    return 0;
+}
